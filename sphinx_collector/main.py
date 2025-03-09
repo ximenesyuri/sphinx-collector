6a9a46c117f9442498e755d145a9051ed776a921
yxm <yxm.dev@gmx.ie>
2025-03-09 16:22:12 -0300

    ...

--- sphinx_collector/main.py
+++ sphinx_collector/main.py
@@ -1,39 +1,39 @@
 import os
-import re
 import yaml
+import re
 
-def setup(app):
-    app.add_config_value('markdown_files', {}, 'env')
-    app.connect('builder-inited', lambda app: None)
+def collect(directory, pattern='*.md'):
+    def extract_frontmatter(content):
+        frontmatter = {}
+        if content.startswith('---'):
+            end = content.find('---', 3)
+            if end != -1:
+                frontmatter = yaml.safe_load(content[3:end].strip())
+                content = content[end + 3:].strip()
+        return frontmatter, content
 
-    global collect
-    collect = _collect
-
-def _collect(directory, pattern='*.md'):
     markdown_files = {}
-    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', directory))
+    base_path = os.path.abspath(directory)
+    conf_py_dir = os.getcwd()
 
     for root, _, files in os.walk(base_path):
         for file_name in files:
             if re.match(pattern.replace('.', r'\.').replace('*', r'.*'), file_name):
-                file_path = os.path.join(root, file_name)
-                with open(file_path, 'r', encoding='utf-8') as file:
+                full_path = os.path.join(root, file_name)
+                relative_path = os.path.relpath(full_path, conf_py_dir)
+                url = os.path.splitext(relative_path)[0]
+                with open(full_path, 'r', encoding='utf-8') as file:
                     content = file.read()
                     frontmatter, content = extract_frontmatter(content)
 
                     markdown_files[file_name] = {
-                        'path': file_path,
+                        'path': {
+                            'full': full_path,
+                            'relative': relative_path,
+                            'url': url,
+                        },
                         'frontmatter': frontmatter,
                         'content': content
                     }
 
     return markdown_files
-
-def extract_frontmatter(content):
-    frontmatter = {}
-    if content.startswith('---'):
-        end = content.find('---', 3)
-        if end != -1:
-            frontmatter = yaml.safe_load(content[3:end].strip())
-            content = content[end + 3:].strip()
-    return frontmatter, content
